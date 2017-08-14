b5af6b79810dd6be641c0e31579d10b1d467e4b8
Zhang Shuai <520zhangshuai@gmail.com>
2017-08-15 00:09:02 +0800

    Add popular libs

--- app/src/main/java/com/thoughtworks/kotlinstartupkit/MainActivity.kt
+++ app/src/main/java/com/thoughtworks/kotlinstartupkit/MainActivity.kt
@@ -2,11 +2,19 @@ package com.thoughtworks.kotlinstartupkit
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
+import butterknife.BindView
+import butterknife.ButterKnife
 
 class MainActivity : AppCompatActivity() {
+
+    @BindView(R.id.main_text) lateinit var mainText: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ButterKnife.bind(this)
+
+        mainText.setText(R.string.app_name)
     }
 }
